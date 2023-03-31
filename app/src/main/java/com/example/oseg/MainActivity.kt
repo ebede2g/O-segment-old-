9ccdbe9d47f3bb5dadc0bea01fa1acbe2b5edc6f
ebede2g <108631764+ebede2g@users.noreply.github.com>
2023-03-31 20:24:13 +0300

    v1.2 OpenGL ES Додано, біндинг не працює

--- app/src/main/java/com/example/oseg/MainActivity.kt
+++ app/src/main/java/com/example/oseg/MainActivity.kt
@@ -1,18 +1,22 @@
 package com.example.oseg
 
+import android.opengl.GLSurfaceView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.oseg.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var mega : ActivityMainBinding                         //біндниг
+    private lateinit var gLView: GLSurfaceView                      //OpenGL
 
 
     override fun onCreate(s: Bundle?) {
         super.onCreate(s)
-        mega = ActivityMainBinding.inflate(layoutInflater)          //наповнюю біндинг
-        setContentView(mega.root)
-        
+        //mega = ActivityMainBinding.inflate(layoutInflater)          //наповнюю біндинг
+        //setContentView(mega.root)
+
+        gLView = MyGLSurfaceView(this)
+        setContentView(gLView)
 
 
     }
